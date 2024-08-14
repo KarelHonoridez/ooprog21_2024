ad4dad85812e370bbc47dd0aa5bc4ae85bef084c
KarelHonoridez <karelhonoridez@gmail.com>
2024-08-14 18:47:44 +0800

    MovieQuoteInfo

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MovieQuoteInfo
+{
+    public static void main (String[] args)
+    {
+        System.out.println("An adaptation of L. Frank Baum's 1900 children's fantasy novel The Wonderful Wizard of Oz");
+        System.out.println("There's no place like home");
+        System.out.println("- Dorothy, 1939");
+    }
+}
