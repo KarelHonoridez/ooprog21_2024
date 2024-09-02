270a268d0032033268059155ecf3e3d48c157ef4
Franzess Karel T. Honoridez <karelhonoridez@gmail.com>
2024-09-02 22:00:46 +0800

    changed

--- Chapter 2/CarlysEventPrice.java
+++ Chapter 2/CarlysEventPrice.java
@@ -10,7 +10,7 @@ public static void main(String[] args)
         int guest = g.nextInt();
         int PRICE_PER_GUEST = 35;
 
-        int total =  guest * pricePerGuest;
+        int total =  guest * PRICE_PER_GUEST;
         System.out.println("***************************************");
         System.out.println("* Carly's makes the food that makes   *");
         System.out.println("*           it a party.               *");
@@ -18,7 +18,7 @@ public static void main(String[] args)
 
 
         System.out.println("Number of guest: " + guest);
-        System.out.println("Price per guest: " + pricePerGuest);
+        System.out.println("Price per guest: " + PRICE_PER_GUEST);
         System.out.println("Total price: " + total);
 
         int largeEventGuest = 50;
