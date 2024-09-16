12bbefefd0097b34c74fa6d1ea7abbd402b24d73
Franzess Karel T. Honoridez <karelhonoridez@gmail.com>
2024-09-16 19:27:55 +0800

    FirstAddressExcercise

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,17 @@
+public class FirstAddressExcercise
+{
+    public static void main (String[] args) 
+    {
+        System.out.println("First Java application"); 
+        displayAddress(); 
+    }
+
+    public static void displayAddress()
+    {
+        System.out.println("Xiaomi");
+        System.out.println("Xiaomi Office Building, 68 Qinghe Middle Street, Beijing");
+        System.out.println("Beijing, 100085");
+        System.out.println("Company: Xiaomi Corporation");
+    }
+
+}
