fc3313e3c973a1d0316654f8d043c92d72d07173
Franzess Karel T. Honoridez <karelhonoridez@gmail.com>
2024-10-02 22:47:12 +0800

    MathTest

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,15 @@
+public calss MathTest {
+ publci static void main ( String [] args) {
+ 
+System.out.println(" : " + Math.sqrt(1025));
+System.out.println(" : " + Math.sin(2));
+System.out.println(" : " + Math.cos(20));
+System.out.println(" : " + Math.floor(25));
+System.out.println(" : " + Math.ceil(10));
+System.out.println(" : " + Math.round(13));
+System.out.println(" : " + Math.max(34,35));
+System.out.println(" : " + Math.min(34,35);
+System.out.println(" : " + Math.random());
+
+ }
+}
