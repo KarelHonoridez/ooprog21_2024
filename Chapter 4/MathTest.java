d4773ff1c82f1823ce68bc3519dc1baa27d3e6b7
Franzess Karel T. Honoridez <karelhonoridez@gmail.com>
2024-10-07 08:24:02 +0800

    Update MathTest.java

--- Chapter 4/MathTest.java
+++ Chapter 4/MathTest.java
@@ -1,15 +1,15 @@
-public calss MathTest {
- publci static void main ( String [] args) {
+public class MathTest {
+ public static void main (String [] args) {
  
-System.out.println("SQR: " + Math.sqrt(1025));
-System.out.println("SIN: " + Math.sin(2));
-System.out.println("COS: " + Math.cos(20));
-System.out.println("FLOOR: " + Math.floor(25));
-System.out.println("CEIL: " + Math.ceil(10));
-System.out.println("ROUND: " + Math.round(13));
-System.out.println("MAX: " + Math.max(34,35));
-System.out.println("MIN: " + Math.min(34,35);
-System.out.println("RANDOM: " + Math.random());
+   System.out.println("SQR: " + Math.sqrt(1025));
+   System.out.println("SIN: " + Math.sin(2));
+   System.out.println("COS: " + Math.cos(20));
+   System.out.println("FLOOR: " + Math.floor(25));
+   System.out.println("CEIL: " + Math.ceil(10));
+   System.out.println("ROUND: " + Math.round(13));
+   System.out.println("MAX: " + Math.max(34,35));
+   System.out.println("MIN: " + Math.min(34,35));
+   System.out.println("RANDOM: " + Math.random());
 
  }
 }
